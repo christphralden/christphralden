097f690b6e94e4d77fc9fe117b2a907d8f41ccf8
christopher-alden <christopheralden04@gmail.com>
2024-06-16 02:42:34 +0700

    nvim configs

---
+++ nvim/lua/christphralden/core/keymaps.lua
@@ -0,0 +1,35 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+keymap.set("i", "jk", "<ESC>")
+
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "x", '"_x"')
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+keymap.set("n", "<leader>sv", "<C-w>v") --split vertical
+keymap.set("n", "<leader>sh", "<C-w>s") -- split horizontal
+keymap.set("n", "<leader>se", "<C-w>=") -- equal window
+keymap.set("n", "<leader>sx", ":close<CR>") -- close
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- newtab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- closetab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- nexttab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --prevtab
+
+-- plugin keymaps
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- nvim tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>j", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>u", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
---
+++ nvim/lua/christphralden/core/options.lua
@@ -0,0 +1,37 @@
+local opt = vim.opt
+
+-- line
+opt.relativenumber = true
+opt.number = true
+
+
+-- indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+
+-- line wrap
+opt.wrap = false
+
+-- search setting
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split widnwos
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
