525fc821cb124f8f85a894bb1caf0bd15d0692b7
christphralden <christopheralden04@gmail.com>
2026-06-06 18:28:15 +0700

    wez

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,6 +10,7 @@ config.debug_key_events = false
 config.adjust_window_size_when_changing_font_size = false
 config.animation_fps = 60
 config.max_fps = 120
+config.prefer_egl = true
 config.scrollback_lines = 2000
 config.initial_rows = 48
 config.initial_cols = 160
@@ -24,68 +25,68 @@ config.font_size = 18
 config.color_scheme = "rose-pine"
 
 config.colors = {
-	cursor_bg = "#e0def4", -- Cursor background color
-	cursor_border = "#e0def4", -- Cursor border color
-	cursor_fg = "#232136", -- Cursor foreground color
-	selection_bg = "#393552", -- Selection background color
-	selection_fg = "#e0def4", -- Selection foreground color
-	ansi = {
-		"#393552", -- Black
-		"#eb6f92", -- Red
-		"#9ccfd8", -- Green
-		"#f6c177", -- Yellow
-		"#3e8fb0", -- Blue
-		"#c4a7e7", -- Magenta
-		"#9ccfd8", -- Cyan
-		"#e0def4", -- White
-	},
-	brights = {
-		"#6e6a86", -- Bright Black
-		"#eb6f92", -- Bright Red
-		"#9ccfd8", -- Bright Green
-		"#f6c177", -- Bright Yellow
-		"#3e8fb0", -- Bright Blue
-		"#c4a7e7", -- Bright Magenta
-		"#9ccfd8", -- Bright Cyan
-		"#e0def4", -- Bright White
-	},
+  cursor_bg = "#e0def4",     -- Cursor background color
+  cursor_border = "#e0def4", -- Cursor border color
+  cursor_fg = "#232136",     -- Cursor foreground color
+  selection_bg = "#393552",  -- Selection background color
+  selection_fg = "#e0def4",  -- Selection foreground color
+  ansi = {
+    "#393552",               -- Black
+    "#eb6f92",               -- Red
+    "#9ccfd8",               -- Green
+    "#f6c177",               -- Yellow
+    "#3e8fb0",               -- Blue
+    "#c4a7e7",               -- Magenta
+    "#9ccfd8",               -- Cyan
+    "#e0def4",               -- White
+  },
+  brights = {
+    "#6e6a86", -- Bright Black
+    "#eb6f92", -- Bright Red
+    "#9ccfd8", -- Bright Green
+    "#f6c177", -- Bright Yellow
+    "#3e8fb0", -- Bright Blue
+    "#c4a7e7", -- Bright Magenta
+    "#9ccfd8", -- Bright Cyan
+    "#e0def4", -- Bright White
+  },
 }
 
 -- Dimmer for background
 local dimmer = {
-	brightness = 0.02,
-	saturation = 0.8,
+  brightness = 0.02,
 }
 
 -- Define the two background options (wallpaper and transparent)
 local background_with_wallpaper = {
-	{
-		source = {
-			File = wez.config_dir .. "/backgrounds/chaewon-1.jpg",
-			-- File = wez.config_dir .. "/backgrounds/chaeyoung-1.jpeg",
-			-- File = wez.config_dir .. "/backgrounds/pain.jpg",
-			-- File = wez.config_dir .. "/backgrounds/azumanga.gif",
-		},
-		width = "Cover",
-		height = "Cover",
-		vertical_align = "Middle",
-		horizontal_align = "Center",
-		repeat_x = "NoRepeat",
-		repeat_y = "NoRepeat",
-		opacity = 1,
-		hsb = dimmer,
-	},
+  {
+    source = {
+      -- File = wez.config_dir .. "/backgrounds/chaewon-1.jpg",
+      -- File = wez.config_dir .. "/backgrounds/chaeyoung-1.jpeg",
+      -- File = wez.config_dir .. "/backgrounds/pain.jpg",
+      -- File = wez.config_dir .. "/backgrounds/azumanga.gif",
+      File = wez.config_dir .. "/backgrounds/matsuya_japan.jpg",
+    },
+    width = "Cover",
+    height = "Cover",
+    vertical_align = "Middle",
+    horizontal_align = "Center",
+    repeat_x = "NoRepeat",
+    repeat_y = "NoRepeat",
+    opacity = 1,
+    hsb = dimmer,
+  },
 }
 
 local transparent_background = {
-	{
-		source = {
-			Color = "#0a0a0a",
-		},
-		width = "100%",
-		height = "100%",
-		opacity = 0.8,
-	},
+  {
+    source = {
+      Color = "#0a0a0a",
+    },
+    width = "100%",
+    height = "100%",
+    opacity = 1,
+  },
 }
 
 -- Initialize with the wallpaper by default
@@ -98,41 +99,41 @@ config.background = background_with_wallpaper
 local current_background = "wallpaper"
 
 wez.on("toggle-background", function(window)
-	if current_background == "wallpaper" then
-		-- Switch to transparent background
-		config.background = transparent_background
-		current_background = "transparent"
-		config.window_background_opacity = 0
-		config.macos_window_background_blur = 30
-	else
-		-- Switch to wallpaper
-		config.background = background_with_wallpaper
-		current_background = "wallpaper"
-		config.window_background_opacity = 1
-		config.macos_window_background_blur = 0
-	end
-	window:set_config_overrides(config)
+  if current_background == "wallpaper" then
+    -- Switch to transparent background
+    config.background = transparent_background
+    current_background = "transparent"
+    config.window_background_opacity = 0
+    config.macos_window_background_blur = 30
+  else
+    -- Switch to wallpaper
+    config.background = background_with_wallpaper
+    current_background = "wallpaper"
+    config.window_background_opacity = 1
+    config.macos_window_background_blur = 0
+  end
+  window:set_config_overrides(config)
 end)
 
 config.keys = {
-	{
-		key = "b",
-		mods = "CTRL|SHIFT",
-		action = wez.action.EmitEvent("toggle-background"),
-	},
-	{
-		key = "Enter",
-		mods = "SHIFT",
-		action = wez.action({ SendString = "\x1b\r" }),
-	},
+  {
+    key = "b",
+    mods = "CTRL|SHIFT",
+    action = wez.action.EmitEvent("toggle-background"),
+  },
+  {
+    key = "Enter",
+    mods = "SHIFT",
+    action = wez.action({ SendString = "\x1b\r" }),
+  },
 }
 
 -- Window options
 config.window_padding = {
-	left = 40,
-	right = 40,
-	top = 40,
-	bottom = 20,
+  left = 10,
+  right = 10,
+  top = 40,
+  bottom = 20,
 }
 
 config.native_macos_fullscreen_mode = false
